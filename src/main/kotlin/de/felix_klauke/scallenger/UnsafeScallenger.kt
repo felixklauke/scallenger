9b8a0450f12bc5e0289b34e2cfd88a38b90310bb
Felix Klauke <info@felix-klauke.de>
2017-12-02 16:04:22 +0100

    Feature/tests (#8)
    * Update and simplify data length check.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add test for scallenger subject.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- src/main/kotlin/de/felix_klauke/scallenger/UnsafeScallenger.kt
+++ src/main/kotlin/de/felix_klauke/scallenger/UnsafeScallenger.kt
@@ -1,6 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Felix Klauke
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package de.felix_klauke.scallenger
 
-import de.felix_klauke.scallenger.subject.ScallengerSubject
 import java.nio.file.Path
 import java.util.*
 
@@ -42,6 +65,6 @@ internal class UnsafeScallenger(dataChunkSize: Long, file: Path) : AbstractScall
      * Compare and swap the integer at the lock position.
      */
     private fun compareAndSwapLock(first: Int, second: Int) : Boolean {
-        return memoryAccessor.compareAndSwapInt(null, bufferAddress + ScallengerConstants.LOCK_OFFSET, first, second);
+        return memoryAccessor.compareAndSwapInt(null, bufferAddress + ScallengerConstants.LOCK_OFFSET, first, second)
     }
 }
